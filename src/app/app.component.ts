128cac7f4a5a8aba1350ceef5e5f4dd015ecdeb9
Gerardo Cortés Oquendo <gerardo.cortes.o@gmail.com>
2024-02-07 21:40:11 +0100

    feat: mock the interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Result} from "../shared/model/Result";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'emi-ui';
+  items : Result[] = [
+    new Result(13598.05),
+    new Result(6585.23)
+  ];
 }
